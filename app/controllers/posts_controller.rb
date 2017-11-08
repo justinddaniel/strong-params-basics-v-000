dcc896217d83136ec1bbca5f39674b536b89f45e
Justin Daniel <justinddaniel@gmail.com>
2017-11-08 23:56:23 +0000

    automatically backed up by learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -16,7 +16,7 @@ class PostsController < ApplicationController
 	  @post.save
 	  redirect_to post_path(@post)
 	end
-	 
+
 	def update
 	  @post = Post.find(params[:id])
 	  @post.update(post_params(:title))
@@ -26,9 +26,9 @@ class PostsController < ApplicationController
 	def edit
 		@post = Post.find(params[:id])
 	end
-	 
+
 	private
-	 
+
 	def post_params(*args)
 	  params.require(:post).permit(*args)
 	end
